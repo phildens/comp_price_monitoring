f6c6663e575c3ef3cf64a3fcdc04e0c8ade04673
phildens <vborodulin67@gmail.com>
2023-11-20 22:43:53 +0500

    rename and create db_file

--- database_connect.py
+++ database_connect.py
@@ -1,4 +1,34 @@
-from pars_part.chaeck_site import get_prices_from_site
+from pars_part.chaeck_site import site_checker
 import sqlite3
-prices = get_prices_from_site()
 
+def create_tables(cur, conn):
+    listOfTables = cur.execute(
+        "SELECT * FROM sqlite_master WHERE type='table'; ").fetchall()
+    print(listOfTables[0][1])
+    if listOfTables == []:
+        print('created')
+
+    cur.execute('DROP TABLE IF EXISTS models')
+
+
+    cur.execute("""
+    CREATE TABLE models (id INTEGER PRIMARY KEY AUTOINCREMENT,
+         name_model VARCHAR(255),
+         price INTEGER)""")
+
+    conn.commit()
+
+#site_info = site_checker()
+file_with_models = open('all_models.txt','r')
+models = file_with_models.readline().split(',')
+# checked_models = []
+# for i in site_info.price_list:
+#     if i[0] in models and not(i[0] in checked_models):
+#         checked_models.append(i)
+# print(checked_models)
+
+
+
+with sqlite3.connect('models_bd.db') as conn:
+    cur = conn.cursor()
+    create_tables(cur, conn)
--- pars_part/chaeck_site.py
+++ pars_part/chaeck_site.py
@@ -5,44 +5,62 @@ from bs4 import BeautifulSoup
 stoper = 'table_body report new_report sticky-th flex-list-tovar descr list list1 hid_tov_tab table_catalog'
 models = ['X', '8', '7', '6', '6S', '11', '5S', 'XR', 'XS', '12', '13', '14']
 diferences = ['Pro', 'Plus', 'Pro Max', 'mini', 'Max', 'Mini']
-def get_prices_from_site():
-    j = 0
+
+
+class site_checker:
     price_list = []
-    while j < 50:
-        request = urllib.request.urlopen(
-            f'https://perm.gsm-opt.ru/zapchasti-dlya-apple/displei/?nalichie_n=0&sortirovka=sales&num_on_page=50&page={j}&vid=0')
-        page = request.read()
-        bsobject = BeautifulSoup(page, 'html.parser')
-        getcontrol = bsobject.find('div', {'class': stoper}).find('p').text
+    unique_list = []
 
-        if getcontrol == 'Ничего не найдено':
-            break
-        get_displays = bsobject.findAll('div', {'class': 'cat_list_el'})
-        for i in get_displays:
-            availability_point = i.find('div', {'class': 'catalog_qty_block'}).text.split()
-            if not (availability_point == ['в', 'наличии']):
-                continue
-            priceobj = i.find('div', {'class': 'table_rozn_price price_td_t active-price_warp'}).find('span', {
-                'class': 'active-price'}).text
+    def __init__(self):
+        self.get_prices_from_site()
+        self.get_unique_models()
 
-            namemodel = i.find('div', {'class' : 'catalog_list_left_block'}).text
-            if len(namemodel) == 0:
-                continue
-            price_num = re.findall(r'\d+', str(priceobj))[0]
-            phonemodel = re.search(r'iPhone ', namemodel)
-            if phonemodel is None:
-                continue
-            phonemodel = phonemodel.group()
+    def get_prices_from_site(self):
+        j = 0
+        self.price_list.clear()
+        while j < 50:
+            request = urllib.request.urlopen(
+                f'https://perm.gsm-opt.ru/zapchasti-dlya-apple/displei/?nalichie_n=0&sortirovka=sales&num_on_page=50&page={j}&vid=0')
+            page = request.read()
+            bsobject = BeautifulSoup(page, 'html.parser')
+            getcontrol = bsobject.find('div', {'class': stoper}).find('p').text
 
-            for model in models:
-                if model in namemodel:
-                    for diferen in diferences:
-                        if diferen in namemodel:
-                            price_list.append([phonemodel + model + ' ' + diferen, int(price_num)])
-                            continue
+            if getcontrol == 'Ничего не найдено':
+                break
+            get_displays = bsobject.findAll('div', {'class': 'cat_list_el'})
+            for i in get_displays:
+                availability_point = i.find('div', {'class': 'catalog_qty_block'}).text.split()
+                if not (availability_point == ['в', 'наличии']):
+                    continue
+                priceobj = i.find('div', {'class': 'table_rozn_price price_td_t active-price_warp'}).find('span', {
+                    'class': 'active-price'}).text
 
-                    price_list.append([phonemodel + model, int(price_num)])
+                namemodel = i.find('div', {'class': 'catalog_list_left_block'}).text
+                if len(namemodel) == 0:
+                    continue
+                price_num = re.findall(r'\d+', str(priceobj))[0]
+                phonemodel = re.search(r'iPhone ', namemodel)
+                if phonemodel is None:
+                    continue
+                phonemodel = 'iPhone '
 
-        j += 1
-    print(price_list)
-    return price_list
+                for model in models:
+                    if model in namemodel:
+                        for diferen in diferences:
+                            if diferen in namemodel:
+                                self.price_list.append([phonemodel + model + ' ' + diferen, int(price_num)])
+                                continue
+
+                        self.price_list.append([phonemodel + model, int(price_num)])
+
+            j += 1
+        return self.price_list
+    def get_unique_models(self):
+        self.unique_list.clear()
+        for i in self.price_list:
+            if not(i[0] in self.unique_list):
+                self.unique_list.append(i[0])
+        return self.unique_list
+
+#test = site_checker()
+#print(test.price_list[0][0])
