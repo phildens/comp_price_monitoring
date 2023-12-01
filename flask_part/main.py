1e15b3eea7f1bc451206a98e442fa46c67632736
phildens <vborodulin67@gmail.com>
2023-12-01 23:14:45 +0500

    repeat reguess

--- flask_part/main.py
+++ flask_part/main.py
@@ -7,25 +7,36 @@ class model:
         self.cost = str(int(coste * 1.25))
         self.realcost = coste
 
-with sqlite3.connect('databas/models_bd.db') as conn:
-    cur = conn.cursor()
-    info_from_bd = cur.execute("""
-    SELECT * FROM models;
-    """).fetchall()
+def connect_users():
+    info_from_bd = []
+    while len(info_from_bd) == 0:
+        try:
+            with sqlite3.connect('databas/models_bd.db') as conn:
+                cur = conn.cursor()
+                info_from_bd = cur.execute("""
+                SELECT * FROM models;
+                """).fetchall()
+        except:
+            pass
+        
+    print(info_from_bd)
+
     
-print(info_from_bd)
+    phone = []
+    for i in info_from_bd:
+        one_model = model(i[1], i[2])
+        phone.append(one_model)
+        
 
-phone = []
-
-for i in info_from_bd:
-    one_model = model(i[1], i[2])
-    phone.append(one_model)
-    print(one_model.model, one_model.cost)
+    print("запрос выполнен")
+    return phone
+    
 
 app = Flask("site_services")
 
 @app.route('/')
 def hello():
+    phone = connect_users()
     return render_template('place.html', iphones = phone)
 
 app.run(host='0.0.0.0')
